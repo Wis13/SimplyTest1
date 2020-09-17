e32688cb51e31b5f1bdd0b4f74f120356eeef905
Vadym Koroliuk <vkqa@outlook.com>
2020-09-16 21:20:10 -0700

    Init

---
+++ src/test/java/Jenkins/simpletest/SimpleTest.java
@@ -0,0 +1,30 @@
+package Jenkins.simpletest;
+
+        import org.junit.After;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    private WebDriver driver;
+    @Before
+    public void SetUp(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        driver = new ChromeDriver();
+    }
+    @After
+    public void TearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void getTitle() throws InterruptedException{
+        driver.navigate().to("https://www.pravda.com.ua/");
+        Thread.sleep(2000);
+        String title = driver.getTitle();
+        assertTrue(title.contains("Українська правда"));
+    }
+}
